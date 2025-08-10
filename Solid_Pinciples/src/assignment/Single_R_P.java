c247a2bed55dd6e29ec1bccb57463d0c1e98f255
ArchanaVenna <154882027+ArchanaVenna@users.noreply.github.com>
2025-08-10 16:07:14 +0530

    Add files via upload

---
+++ Solid_Pinciples/src/assignment/Single_R_P.java
@@ -0,0 +1,28 @@
+package assignment;
+
+//one class=one job
+class Student{
+	String name;
+	
+	Student(String name){
+		this.name=name;
+	}
+}
+
+class Print{
+	void print(Student s) {
+		System.out.println("Name is : "+s.name);
+	}
+}
+
+public class Single_R_P {
+
+	public static void main(String[] args) {
+		
+		Student s=new Student("Archana");
+		Print p=new Print();
+		p.print(s);
+		
+	}
+
+}
